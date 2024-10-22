26277cedd918eab0e94899a552f488fa2678dd0f
rq-katou-kaede-at-321899771753 <k.mpy0926@gmail.com>
2024-10-22 23:09:37 +0900

    アカウント設定機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [profile_attributes: [:name] ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [profile_attributes: [:name]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password])
   end
 end
