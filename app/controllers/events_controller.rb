e55d87c805984b0be92c1b26c2a6d0f80d50443e
rq-katou-kaede-at-321899771753 <k.mpy0926@gmail.com>
2024-10-25 23:18:29 +0900

    Participantsテーブル作成、イベント一覧ページ実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,18 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create]
+  before_action :authenticate_user!, only:[:new, :create, :update, :destroy, :toggle_registration]
+  before_action :set_event, only: [:show, :edit, :update, :destroy, :toggle_registration, :toggle_participation]
+
+  def index
+    @events = Event.all.select(&:registration_open?)
+  end
+
   def new
     @event = Event.new
   end
 
   def create
     @event = current_user.events.build(event_params)
+    @event.user_id = current_user.id
     if @event.save
       redirect_to @event, notice: 'イベントが作成されました'
     else
@@ -13,9 +20,66 @@ class EventsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+    if @event.update(event_params)
+      @event.update_registration_status
+      redirect_to  @event, notice: 'イベントが更新されました'
+    else
+      flash.now[:alert] = 'イベントの更新に失敗しました'
+      render :edit
+    end
+  end
+
+  def toggle_registration
+    if @event.hosted_by?(current_user)
+      if @event.registration_open?
+        @event.close_registration
+      else
+        if @event.date.future? && (@event.capacity.nil? || @event.capacity > @event.participants.count)
+          @event.open_registration
+        else
+          flash[:alert] = "過去のイベントまたは定員に達したイベントのため、再開できません"
+          render events_path
+        end
+      end
+    end
+
+    @button_container_id = "button_container_#{@event.id}"
+    respond_to do |format|
+      format.turbo_stream { render 'toggle_button' }
+      format.html { redirect_to events_path }
+    end
+  end
+
+  def toggle_participation
+    if current_user.participating?(@event)
+      current_user.leave_event(@event)
+    else
+      current_user.join_event(@event)
+    end
+    @button_container_id = "button_container_#{@event.id}"
+    respond_to do |format|
+      format.turbo_stream { render 'toggle_button' }
+      format.html { redirect_to events_path }
+    end
+  end
+
   private
 
   def event_params
     params.require(:event).permit(:name, :date, :location, :capacity, :visibility, :group_id, :description)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 end
