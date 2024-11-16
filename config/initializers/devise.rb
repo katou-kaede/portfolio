bc4181272636bab08d0375ee8eb7601dc6094bdb
rq-katou-kaede-at-321899771753 <k.mpy0926@gmail.com>
2024-11-16 16:45:09 +0900

    fix: LINEログイン

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -276,7 +276,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :line, ENV['LINE_KEY'], ENV['LINE_SECRET']
+  config.omniauth :line, ENV["LINE_KEY"], ENV["LINE_SECRET"], callback_url: 'https://event-station.onrender.com/users/auth/line/callback'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
