57f6b98dc0e7e912bb6aea60bca28eb29afdef35
rq-katou-kaede-at-321899771753 <k.mpy0926@gmail.com>
2024-11-23 14:35:39 +0900

    タイピングミス修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     registrations: "users/registrations",
     passwords: "users/passwords",
-    omniauth_callbacks: "omniauth_callbacks"
+    omniauth_callbacks: "omniauth_callbacks",
     current_uid: "/api/current_uid"
   }
 
